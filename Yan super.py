959dddc08173aba8fca26e1e71e5c8f8c9ca76bf
ian-fisher <yanfisher13@gmail.com>
2016-10-22 20:01:23 +0300

    Update Yan super.py

--- Yan super.py
+++ Yan super.py
@@ -1,1 +1,22 @@
+задание на етот раз такое:
+import tkinter
 
+def plus():
+    lb = tkinter.Label(root, width = 400, height = 400, text = 'Hello World')
+    lb.pack()
+    return lb
+
+def plus2():
+    print('Hello world')
+    
+root = tkinter.Tk()
+root.title('Game PRO')
+root.minsize(width = 400, height = 400)
+root.maxsize(width = 400, height = 400)
+
+but = tkinter.Button(root, text = 'Hello World', command = plus)
+but2 = tkinter.Button(root, text = 'Hello World', command = plus2)
+but2.pack(fill = 'x')
+but.pack(fill = 'y')
+
+root.mainloop()
