35e0e79b136dc86950809e51c1c5391548e3e752
ian-fisher <yanfisher13@gmail.com>
2016-11-12 19:41:36 +0200

    Update Yan super.py3.py

--- Yan super.py3.py
+++ Yan super.py3.py
@@ -1,27 +1,31 @@
- import tkinter
+import tkinter
    
 def complete():
-    n = int(textbox.get())
-    for i in range(1, n):
-        result = 2**i
+    if textbox.get() == '':
+        var.set('Field to empty')
+    elif len(textbox.get()) >= 2:
+        var.set('Too many symbvols')
+    else:
+        n = int(textbox.get())
+        result = [int(textpack.get())**i for i in range(1, n+1)]
+        var.set(result)
 
-    label = tkifill = xnter.Label(root, width = 100, text = result)
-    label.pack()
-    return label
 
 root = tkinter.Tk()
 root.title('Number')
 root.minsize(width = 400, height = 400)
 root.maxsize(width = 600, height = 600)
 
-textbox = tkinter.Entry(root, width = 100)
-value = textbox.get()
-print(value)
-butComplete = tkinter.Button(root, text = 'Ok', width = 100 command = complete)
+var = tkinter.StringVar()
 
+textpack = tkinter.Entry(root, width = 50)
+textbox = tkinter.Entry(root, width = 100)
+butComplete = tkinter.Button(root, text = 'Ok', width = 100, command = complete)
+label = tkinter.Label(root, width = 100, textvariable = var)
+
+textpack.pack(fill= 'x')
 textbox.pack(fill = 'x')
 butComplete.pack(fill = 'x')
-s = v.get()
+label.pack(fill='x')
 
 root.mainloop()
-
