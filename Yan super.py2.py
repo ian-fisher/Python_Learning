70acac92187414b01f172ab19ca0707b7ea821d3
ian-fisher <yanfisher13@gmail.com>
2016-11-12 20:01:40 +0200

    Update Yan super.py2.py

--- Yan super.py2.py
+++ Yan super.py2.py
@@ -1,3 +1,32 @@
-def hello(name):
-    return name(int(input()))
-print('Hello' + hello())
+import tkinter
+   
+def complete():
+    if textbox.get() == '':
+        var.set('Field to empty')
+    elif len(textbox.get()) >= 2:
+        var.set('Too many symbvols')
+    else:
+        n = int(textbox.get())
+        result = [int(textpack.get())**i for i in range(1, n+1)]
+        var.set(result)
+
+
+root = tkinter.Tk()
+root.title('Number')
+root.minsize(width = 400, height = 400)
+root.maxsize(width = 600, height = 600)
+
+var = tkinter.StringVar()
+
+textpack = tkinter.Entry(root, width = 50)
+textbox = tkinter.Entry(root, width = 50)
+butComplete = tkinter.Button(root, text = 'Ok', width = 100, command = complete)
+label = tkinter.Label(root, width = 100, textvariable = var)
+
+textpack.grid(row = 0, column = 0)
+textbox.grid(row = 0, column = 1)
+butComplete.pack(fill = 'x')
+label.pack(fill='x')
+
+root.mainloop()
+
