b9cf2b168c5b223ad3f7a7a351ec682afefa8d72
ian-fisher <yanfisher13@gmail.com>
2016-11-19 19:59:48 +0200

    Update Yan super.py1.py

--- Yan super.py1.py
+++ Yan super.py1.py
@@ -1,48 +1,31 @@
+import tkinter
 
 
-countx = 0
-count0 = 0
-flag = False
-
-
-def xo(flag, countx, count0):
-    if flag == False:
-        data.set('X')
-        countx += 1
-        flag = True
+def save():
+    nameFile = fileBox.get()
+    txt = fieldBox.get("1.0",'end-1c')
+    if nameFile != '':
+        file = open(nameFile, mode = 'w')
+        file.write(txt)
+        file.close()
+        print('Done write!')
     else:
-        data.set('0')
-        count0 += 1
-    flag = False
-        
+        print('Error! Not Name File!')
+
+
 root = tkinter.Tk()
-root.title('X and 0')
-root.minsize(width = 800, height = 900)
-root.maxsize(width = 800, height = 900)
+root.title('Notepad')
+root.minsize(width = 500, height = 500)
+root.maxsize(width = 500, height = 10
+             00)
 
-data = tkinter.StringVar()
+fileBox = tkinter.Entry(root, width = 53)
+fieldBox = tkinter.Text(root, width = 60,height = 40)
+buttonSave = tkinter.Button(root, width = 60,height = 1,text = 'SAVE',command = save)
 
-but1 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but2 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but3 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but4 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but5 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but6 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but7 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but8 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-but9 = tkinter.Button(root, width = 100, height = 5, textvariable = data, command = xo(flag, countx, count0))
-
-but1.pack()
-but2.pack()
-but3.pack()
-
-but4.pack()
-but5.pack()
-but6.pack()
-
-but7.pack()
-but8.pack()
-but9.pack()
+fileBox.grid()
+fieldBox.grid()
+buttonSave.grid()
 
 root.mainloop()
 
